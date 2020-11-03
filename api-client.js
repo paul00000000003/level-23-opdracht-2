58cae7def080ea85e22d127d24a3fa86683e8046
Paul van Mierlo <pauljozefvanmierlo@yahoo.com>
2020-11-03 13:39:28 +0100

    movies 2

--- api-client.js
+++ api-client.js
@@ -1,11 +1,13 @@
-const API_KEY = "5cf6a1e410784fee2bab09973e002a25";
+const API_KEY = "602151ee0515babb6cbb286da27be73b";
 
 async function getData() {
     try {
-
-        let apiUrl = "https://api.themoviedb.org/3/movie/550?api_key=" + API_KEY;
-        let res = await fetch(apiUrl);
-        res = await res.json();
-        console.log(res);
+        //let apiUrl = "   ?api_key=" + API_KEY;
+        //let text = await fetch("https://jsonplaceholder.typicode.com/users");
+        let text = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_KEY);
+        let result = await text.json();
+        const namen = result["genres"].map(element => element.name);
+        const ids = result["genres"].map(element => element.id);
+        return [namen, ids];
     } catch (err) { console.log(err) }
 }
--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 async function verkrijgData() {
-    await getData();
+    let genres = await getData();
+    let namen = genres[0];
+    let ids = genres[1];
+    //console.log(genres[0]);
+    return genres;
 }
 
-verkrijgData();
+async function postMoviesGenres(namen, ids) {
+    namen.forEach((element, index) => {
+        console.log(element + " " + ids[index]);
+        regel = document.createElement("li");
+        regel.innerHTML = "genre naam : " + element + " ,id " + ids[index];
+        genresLijst.appendChild(regel);
+    })
+
+}
+
+async function postGetTopRatedMovies() {
+    console.log("binnen postGetTopRatedMovies");
+}
+
+async function postGetTopRatedActionMovies() {
+    console.log("binnen postGetTopRatedActionMovies");
+}
+
+let genresLijst = document.getElementById("genres");
+let regel = "";
+let arr3 = [];
+let text = verkrijgData().then(result => {
+    let genres = result;
+    let namen = genres[0];
+    let ids = genres[1];
+    //let ids = genres[1];
+    postMoviesGenres(namen, ids);
+    postGetTopRatedMovies();
+    postGetTopRatedActionMovies();
+    arr3 = result;
+});
+
+setTimeout(() => console.log(arr3.length), 200);
